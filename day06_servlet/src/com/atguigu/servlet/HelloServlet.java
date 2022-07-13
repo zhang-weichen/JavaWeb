c37d9c9ccc24dd8ca030f3c931a2491b4684070b
ZhangWeichen <734676881@qq.com>
2022-07-13 17:30:15 +0800

    keep learning

--- day06_servlet/src/com/atguigu/servlet/HelloServlet.java
+++ day06_servlet/src/com/atguigu/servlet/HelloServlet.java
@@ -1,6 +1,7 @@
 package com.atguigu.servlet;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 /**
@@ -9,9 +10,13 @@ import java.io.IOException;
  */
 public class HelloServlet implements Servlet {
 
+    public HelloServlet() {
+        System.out.println("1. 执行Servlet构造器方法");
+    }
+
     @Override
     public void init(ServletConfig servletConfig) throws ServletException {
-
+        System.out.println("2. 执行init初始化方法");
     }
 
     @Override
@@ -28,7 +33,23 @@ public class HelloServlet implements Servlet {
      */
     @Override
     public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        System.out.println("3. 执行service方法");
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+        String method = httpServletRequest.getMethod();
 
+        if ("GET".equals(method)) {
+            doGet();
+        } else if ("POST".equals(method)){
+            doPost();
+        }
+    }
+
+    private void doGet() {
+        System.out.println("get请求");
+    }
+
+    private void doPost() {
+        System.out.println("post请求");
     }
 
     @Override
@@ -38,6 +59,6 @@ public class HelloServlet implements Servlet {
 
     @Override
     public void destroy() {
-
+        System.out.println("4. 执行detroy销毁方法");
     }
 }
